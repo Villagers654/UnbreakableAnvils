e1e4685824aaa254509f2b8bf4a5a2ed3bd9f613
Villagers654 <thbvill@gmail.com>
2023-04-26 19:41:32 +0900

    Prevent BlockBreakEvent on an unbreakable anvil

--- src/main/java/club/aurorapvp/unbreakableanvils/events/AnvilEvents.java
+++ src/main/java/club/aurorapvp/unbreakableanvils/events/AnvilEvents.java
@@ -1,10 +1,12 @@
 package club.aurorapvp.unbreakableanvils.events;
 
+import club.aurorapvp.unbreakableanvils.configs.Lang;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
@@ -25,6 +27,29 @@ public class AnvilEvents implements Listener {
 
         assert locString != null;
         if (locString.equals(blockLoc.toString())) {
+          event.setCancelled(true);
+        }
+      }
+    }
+  }
+
+  @EventHandler
+  public void onAnvilBreak(BlockBreakEvent event) {
+    if (event.getBlock().getType() != Material.ANVIL) {
+      return;
+    }
+
+    Location blockLoc = event.getBlock().getLocation();
+    PersistentDataContainer container = event.getBlock().getWorld().getPersistentDataContainer();
+
+    for (NamespacedKey key : container.getKeys()) {
+      if (key.getKey().startsWith("ua")) {
+        String locString = container.get(key, PersistentDataType.STRING);
+
+        assert locString != null;
+        if (locString.equals(blockLoc.toString())) {
+          event.getPlayer().sendMessage(Lang.getComponent("break-denied"));
+
           event.setCancelled(true);
         }
       }
